494771f119dd4e6dc7a47982c469671a5165b370
munna <munnavaibba408@gmail.com>
2023-03-21 23:15:09 +0600

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
 
+const number = 5555;
+const singer = {name: 'Dr. Mahfuz', job: 'singer'}
+
+const singerStyle = {
+  color: 'red', 
+  backgroundColor: 'white',
+  padding: 20 ,
+}
+
 function App() {
+  const nayoks = ['Rubel', 'BappaRaz', 'Kuber' , 'jashim', 'salmnshah', 'kanchon',  'amit',];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        // nayoks.map(nayok => <li>Name: {nayok}</li>)
+      }
+      {
+        nayoks.map(nayok => <Person name={nayok}></Person>)
+      }
+      {/* <Person name={nayoks[0]} nayika="Moushumi"></Person>
+      <Person name={nayoks[1]} nayika="cheka"></Person>
+      <Person name={nayoks[2]} nayika="kopila"></Person> */}
+      
+
+    <h5>New component YAY</h5>
+    <p id='totocompany'>Rock mama React</p>
+    <Friend name="Basar" phone="01514123456" ></Friend>
+    <Friend name="Shakil" phone="01303030303"></Friend>
+    <Friend name="Mustafiz" phone="01303030303"></Friend>
+    <Friend name="Arif" phone="01303030303"></Friend>
+    <Friend name="Islam" phone="01303030303"></Friend>
+    <Friend name="Rajib" phone="01303030303"></Friend>
+    <Friend name="Milon" phone="01303030303"></Friend>
     </div>
   );
+}
+
+function Person(props){
+
+  return (
+    <div className='person'>
+    <h1>{props.name}</h1>
+    <p>{props.nayika}</p>
+  </div>
+  )
+}
+
+function Friend(props){
+  console.log(props);
+  return(
+    <div className='container'>
+      <h1>{props.name}</h1>
+      <p> Phone: {props.phone}</p>
+
+    </div>
+  )
 }
 
 export default App;
